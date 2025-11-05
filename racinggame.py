99303c4275fd72006ce8a85034044f02910ebf55
I-L9614 <leviitamar034@gmail.com>
2025-11-05 11:36:27 +0200

    race

--- racinggame.py
+++ racinggame.py
@@ -56,5 +56,56 @@ class Motorcycle(Car):
     def weave(self):
         ran = random.randint(0, 20)
         self.position += ran
-        
-           
+
+class Race:
+    def __init__(self, track_lengh = 1000):
+        self.track_lengh = track_lengh
+        self.cars = []
+        self.finished = False
+    
+    def add_car(self, car):
+        self.cars.append(car)
+    
+    def show_position(self):
+        for car in self.cars:
+            print(f"car: {car.name} position: {car.position}")
+    
+    def race_turn(self):
+        for car in self.cars:
+            num = random.randint(0,3)
+            for i in range(num):
+                car.accelerate()
+            car.move()
+    
+    def check_winner(self):
+        for car in self.cars:
+            if car.position >= self.track_lengh:
+                return car.name
+        return None
+
+    def start_race(self):
+        turn = 1
+        while self.check_winner() is None:
+            print(f'turn: {turn}')
+            self.race_turn()
+            self.show_position()
+            turn += 1
+        print(f"Winner: {self.check_winner()}")
+ 
+
+def main():
+    print("Welcom")
+    race = Race()
+    regulr_kilear = Car('regulr_kilear', 70)
+    the_wind_cutter = SportCar('the_wind_cutter ')
+    the_havy_weight = Truck('the_havy_weight')
+    the_little_assassin = Motorcycle('the_little_assassin')
+    race.add_car(regulr_kilear)
+    race.add_car(the_wind_cutter)
+    race.add_car(the_havy_weight)
+    race.add_car(the_little_assassin)
+    race.show_position()
+    race.start_race()
+    
+main()
+
